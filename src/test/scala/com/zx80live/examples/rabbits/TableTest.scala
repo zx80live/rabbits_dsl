a06b03978805a7a34a699130e394e92b1bbb0978
Andrew Proshkin <zx80live@gmail.com>
2019-01-18 11:04:46 +0300

    update tests

--- src/test/scala/com/zx80live/examples/rabbits/TableTest.scala
+++ src/test/scala/com/zx80live/examples/rabbits/TableTest.scala
@@ -16,6 +16,20 @@ class TableTest extends FunSuite with Matchers {
     (Table() | "A" | "B" | "C").rows shouldEqual Vector(Row("A", "B", "C"))
   }
 
+  test("|") {
+    Table().rows shouldEqual Vector.empty
+    (Table() | "A").rows shouldEqual Vector(Row("A"))
+    (Table() | "A" | "B" | "C").rows shouldEqual Vector(Row("A", "B", "C"))
+    (Table() | "A" || "B" | "C").rows shouldEqual Vector(Row(Vector(Cell("A", 2))), Row("B", "C"))
+    (Table() | "A" || "B" | "C" | "D").rows shouldEqual Vector(Row(Vector(Cell("A", 3))), Row("B", "C", "D"))
+  }
+
+  test("||") {
+    Table().rows shouldEqual Vector.empty
+    (Table() || "A").rows shouldEqual Vector(Row("A"))
+    (Table() || "A" || "B").rows shouldEqual Vector(Row("A"), Row("B"))
+  }
+
   test("toString") {
     Table().toString shouldEqual "<table border=\"1\"><tr><td><pre>&nbsp;</pre></td></tr></table>"
     (Table() | "A" | "B" | "C" || "D" | "E" || "F").toString shouldEqual "<table border=\"1\"><tr><td><pre>A</pre></td><td><pre>B</pre></td><td><pre>C</pre></td></tr><tr><td><pre>D</pre></td><td colspan=\"2\"><pre>E</pre></td></tr><tr><td colspan=\"3\"><pre>F</pre></td></tr></table>"
